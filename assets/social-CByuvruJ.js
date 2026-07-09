86b22ada0bd23f26584897377a42d261e18c25ad
kytonauts <kytonauts@proton.me>
2026-07-09 08:59:04 +0200

    up

--- assets/social-CByuvruJ.js
+++
@@ -1,1 +0,0 @@
-import{j as m}from"./styles-DpFLYk1c.js";import{_ as p}from"./SocialApp-DkSsS5M0.js";m(p).mount("#app");
---
+++ assets/social-DxIirElp.js
@@ -0,0 +1,1 @@
+import{j as m}from"./styles-dchUc2tz.js";import{_ as p}from"./SocialApp-D7tgV3pH.js";m(p).mount("#app");
